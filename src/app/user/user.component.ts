6db5ffa45f9d93be97a411cb22043f5b53a8d0af
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-09-28 03:13:57 +0000

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  username = input();
+  age = input();
+  nationality = input();
+}
